12637fbc8eb76315549b751c53c837297040fcc2
bitfree <bitfree@gmail.com>
2015-11-18 21:23:03 +0900

    add lockup

---
+++ lockup/lockup.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+
+static int lockup_init(void)
+{
+	for(;;)
+		;
+	
+	return 0;
+}
+
+static int lockup_exit(void)
+{
+
+}
+
+module_init(lockup_init);
+module_exit(lockup_exit);
+
+
