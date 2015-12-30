74163865960d2a09edfba23d7c0d22402ef57767
bitfree <bitfree@gmail.com>
2015-12-30 11:40:16 +0900

    ck369 ver 0.1

---
+++ python/test/ck369.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# @author : bitfree@gmail.com
+# Ver : 0.1
+#
+# this script check how many clap 369 number from start to end
+
+
+import sys
+import getopt
+
+L = [0,]
+def ck369(a):
+	clap = 0
+	clap = clap + str(a).count('3') + str(a).count('6') + str(a).count('9')
+	return int(clap)
+
+for k in xrange(int(sys.argv[1]),int(sys.argv[2])):
+	L.append(ck369(k))
+
+print sum(L) 
