061f05a3e2d7f699d3863681c268a0d6cb560f89
bitfree <bitfree@gmail.com>
2015-12-30 11:35:44 +0900

    speed up : check 369 number in function

--- python/test/threesixnine.py
+++ python/test/threesixnine.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
+# @author : bitfree@gmail.com
+# Ver : 0.2
+#
+# this script check how many clap 369 number from start to end
+# 2015.12.29
+# Improvement : speed up check 369 numbers in function
+
 import sys
 import getopt
+
 
 
 def check369(a,b):
@@ -9,7 +17,6 @@ def check369(a,b):
 	for c in [str(x) for x in xrange(a,b+1)]:
 		clap = clap + c.count('3') + c.count('6') + c.count('9')
 	print clap
-
 
 check369(int(sys.argv[1]),int(sys.argv[2]))
 
