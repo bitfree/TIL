a08bbd062e96e72873a378518cb25a16b98adefe
bitfree <bitfree@gmail.com>
2015-12-30 09:12:20 +0900

    make indent tab=4

---
+++ python/test/threesixnine.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import getopt
+
+
+def check369(a,b):
+	clap = 0
+	for c in [str(x) for x in xrange(a,b+1)]:
+		clap = clap + c.count('3') + c.count('6') + c.count('9')
+	print clap
+
+a=int(sys.argv[1])
+b=int(sys.argv[2])
+
+check369(int(sys.argv[1]),int(sys.argv[2]))
+
