89f643884ef9c509498386ef917c3756a893840a
gichoi <bitfree@gmail.com>
2022-02-05 17:01:52 +0900

    modify urllib_test_https.py

--- python/test/urllib_test_https.py
+++ python/test/urllib_test_https.py
@@ -3,3 +3,11 @@ from bs4 import BeautifulSoup
 import ssl
 
 
+context = ssl._create_unverified_context()
+result = urlopen("https://en.wikipedia.org/wiki/Kevin_Bacon", context=context)
+bsObj = BeautifulSoup(result.read(), "html.parser")
+
+for tag in bsObj.findAll("a"):
+    if "href" in tag.attrs:
+        print(tag.attrs["href"])
+
